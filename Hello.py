1339c05d59c07dd775968add7436d8989d7662a9
JcGit5 <117119209+JcGit5@users.noreply.github.com>
2022-10-31 17:01:39 -0300

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,22 @@
+import numpy as num
+import numpy as np
+#import pandas as pd
+import matplotlib.pyplot as plt
+import queue
+import openpyxl
+
+
+print("Hello")
+
+name='Test'
+
+print(name + "  world")
+
+
+def funCheck():
+    print("Hello" + name)
+
+funCheck()
+
+name=input("Type your name")
+funCheck()
